607bcd1d73a42daa5cd3c6180113ee280cfc0b98
alksharm <alka.sharma@oracle.com>
2016-02-20 18:35:29 +0530

    using bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ListManager = require('./components/ListManager.jsx');
 
-ReactDOM.render(<ListManager title="Ingredients"/>, document.getElementById('ingredients'));
+ReactDOM.render(<ListManager title="Ingredients" headingColor="#FF69B4"/>, document.getElementById('ingredients'));
+ReactDOM.render(<ListManager title="ToDo" headingColor="#007F00"/>, document.getElementById('todo'));
+ReactDOM.render(<ListManager title="Holi" headingColor="#e52d27"/>, document.getElementById('holi'));
